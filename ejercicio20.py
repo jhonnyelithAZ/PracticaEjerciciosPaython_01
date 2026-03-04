fe25bba433f56a84080a8eab934b4ecfeeb7a51f
jhonatan ayala <jhonatanayala3478@gmail.com>
2026-03-04 15:55:43 -0500

    actualice el ejercicio 20

--- ejercicio20.py
+++ ejercicio20.py
@@ -1,7 +1,7 @@
 edad=int(input("escriba su edad :"))
 estrato=int(input("escriba su estrato :"))
 
-if edad>=18 and edad<=25 and estrato==1 or estrato==2 or estrato==3:
+if (edad>=18 and edad<=25) and (estrato==1 or estrato==2 or estrato==3):
     print("aplica subsidio")
 
 else:
